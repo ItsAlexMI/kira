b2bf1512af93563e432ce4199eb508825c74a459
CupOfCoffe <edwinjsa2021@gmail.com>
2025-10-17 06:24:05 -0600

    :bug: verificando las url de las imagenes de los lugares

--- src/utils/places.js
+++ src/utils/places.js
@@ -5,7 +5,7 @@ export const places = [
         coordinate: { latitude: 12.139444, longitude: -86.271389 },
         title: 'Loma de Tiscapa',
         description: 'Histórico *mirador natural* en el borde de una laguna de cráter, con vista panorámica de Managua. Alberga las ruinas del antiguo *Palacio Presidencial* y la icónica *estatua del General Sandino*.',
-        image: "https://pub-dc88437b1eb54374b54af12eb03423a9.r2.dev/loma%20tiscapa.jpg",
+        image: "https://media-cdn.tripadvisor.com/media/photo-s/10/94/e0/52/loma-de-tiscapa.jpg",
         highlight: { primary: 'Managua, ', secondary: 'Loma de Tiscapa' },
         images: [
             "https://tse1.mm.bing.net/th/id/OIP.2fUYX9zOWTpStD7M0a4t_AHaFj?cb=12&rs=1&pid=ImgDetMain&o=7&rm=3",
@@ -32,7 +32,7 @@ export const places = [
         coordinate: { latitude: 12.339444, longitude: -86.021944 }, // 12°20′22″N 86°01′19″O
         title: 'Hacienda San Jacinto',
         description: '*Monumento nacional* que conmemora la histórica *Batalla de San Jacinto* (1856), donde tropas nicaragüenses derrotaron a los filibusteros. Es un *símbolo patrio* de la resistencia y soberanía nacional.',
-        image: "https://pub-dc88437b1eb54374b54af12eb03423a9.r2.dev/Hacienda-San-Jacinto_tipitapa.jpg",
+        image: "https://tse1.mm.bing.net/th/id/OIP.erqvs0yAyLpIjoxzivA7cwHaFj?cb=12&rs=1&pid=ImgDetMain&o=7&rm=3",
         highlight: { primary: 'Tipitapa, ', secondary: 'Hacienda San Jacinto' },
         images: [
             "https://tse2.mm.bing.net/th/id/OIP.LQiEyU-lUhuyDyk6mKJs9wHaE9?cb=12&rs=1&pid=ImgDetMain&o=7&rm=3",
@@ -47,7 +47,7 @@ export const places = [
         coordinate: { latitude: 12.1497, longitude: -86.3400 }, 
         title: 'Monumento a Carlos Fonseca',
         description: 'Monumento en honor a *Carlos Fonseca Amador, fundador del FSLN y figura clave en la historia moderna de Nicaragua. Refleja la **memoria revolucionaria y popular* del municipio.',
-        image: "https://pub-dc88437b1eb54374b54af12eb03423a9.r2.dev/carlos%20fonseca%20amador.jpeg",
+        image: "https://canal2tv.com/wp-content/uploads/2023/11/ciudad-sandino.jpg",
         highlight: { primary: 'Ciudad Sandino, ', secondary: 'Monumento a Carlos Fonseca' },
         images: [
             "https://tse4.mm.bing.net/th/id/OIP.2A9HlvZ7K9Jmz1ifXyv3mQHaEK?cb=12&rs=1&pid=ImgDetMain&o=7&rm=3",
@@ -62,7 +62,7 @@ export const places = [
         coordinate: { latitude: 11.9860, longitude: -86.3120 }, 
         title: 'Mirador Puertas del Cielo',
         description: 'Mirador famoso por su *vista panorámica* del Lago de Managua, la ciudad y el paisaje circundante. Un *lugar de esparcimiento* con clima fresco de montaña y gran belleza natural.',
-        image: "https://pub-dc88437b1eb54374b54af12eb03423a9.r2.dev/el%20crucero%20mirador.webp",
+        image: "https://tse1.mm.bing.net/th/id/OIP.qY4UZGWpYe5X8kWuMGRTyQHaEK?cb=12&rs=1&pid=ImgDetMain&o=7&rm=3",
         highlight: { primary: 'El Crucero, ', secondary: 'Mirador Puertas del Cielo' },
         images: [
             "https://tse4.mm.bing.net/th/id/OIP.M6-aj3oXmLs8nFrxPNrRHQHaEK?cb=12&rs=1&pid=ImgDetMain&o=7&rm=3",
@@ -77,7 +77,7 @@ export const places = [
         coordinate: { latitude: 12.435017, longitude: -86.878056 }, // 12°26′06″N 86°52′41″O
         title: 'Catedral de León (Basílica de la Asunción)',
         description: '*Patrimonio de la Humanidad UNESCO, es la obra arquitectónica más importante y el mayor templo por extensión de Centroamérica. Su estilo es una transición del barroco al neoclásico. Famosa por sus **criptas y la azotea blanca*.',
-        image: "https://pub-dc88437b1eb54374b54af12eb03423a9.r2.dev/catedral%20de%20leon.webp",
+        image: "https://tse2.mm.bing.net/th/id/OIP.duSW0JAE06e_VfVivhjPWwHaEK?cb=12&rs=1&pid=ImgDetMain&o=7&rm=3",
         highlight: { primary: 'León, ', secondary: 'Catedral de la Asunción' },
         images: [
             "https://pueblosdenicaragua.com/wp-content/uploads/2023/05/historia-de-la-catedral-de-leon-nicaragua-768x432.jpg",
@@ -91,7 +91,7 @@ export const places = [
         coordinate: { latitude: 12.396944, longitude: -86.610000 }, // 12°23′49″N 86°36′36″O - Ruinas de León Viejo
         title: 'Ruinas de León Viejo',
         description: '*Patrimonio de la Humanidad UNESCO, este sitio es el asentamiento original de la ciudad de León, fundado en 1524 y abandonado en 1610 tras una serie de sismos. Es un excepcional testimonio de una de las **primeras ciudades coloniales españolas* en América.',
-        image: "https://pub-dc88437b1eb54374b54af12eb03423a9.r2.dev/leon%20viejo.webp",
+        image: "https://diariobarricada.com/wp-content/uploads/2022/11/b6e14639-2c8c-4d1b-aa61-750d01e7e694-1024x684.jpg",
         highlight: { primary: 'León, ', secondary: 'Ruinas de León Viejo' },
         images: [
             "https://th.bing.com/th/id/R.820de0a4152701a79fb28fe3ca310a37?rik=Ifjf3oxd0NIreQ&riu=http%3a%2f%2fwww.altiplano-voyage.com%2fassets%2fnicaragua%2fsites-a-visiter%2fruines-de-leon-viejo-unesco_1_croped.jpg%3f1641812202&ehk=fbQZVRTXQADobkQZdxaIWhK3zh%2b2V2Hf%2fVymweyNz3s%3d&risl=&pid=ImgRaw&r=0",
@@ -105,7 +105,7 @@ export const places = [
         coordinate: { latitude: 12.436389, longitude: -86.877500 }, // Coordenada aproximada de la Casa-Museo Rubén Darío
         title: 'Casa-Museo Rubén Darío',
         description: 'Hogar donde creció el príncipe de las letras castellanas, *Rubén Darío. Convertido en museo, conserva objetos y documentos que ilustran la vida y obra del poeta, siendo un **sitio fundamental para la literatura* e identidad nicaragüense.',
-        image: "https://pub-dc88437b1eb54374b54af12eb03423a9.r2.dev/casa%20museo%20ruben%20dario%20leon.webp",
+        image: "https://media-cdn.tripadvisor.com/media/photo-s/0d/e0/0e/ce/inside-the-museum.jpg",
         highlight: { primary: 'León, ', secondary: 'Casa-Museo Rubén Darío' },
         images: [
             "https://tse4.mm.bing.net/th/id/OIP.XVFs7x8IXqm5oXdgIgqfxgHaFj?cb=12&rs=1&pid=ImgDetMain&o=7&rm=3",
@@ -120,7 +120,7 @@ export const places = [
         coordinate: { latitude: 11.932222, longitude: -85.951944 }, // 11°55′56″N 85°57′07″O
         title: 'Centro Cultural Convento San Francisco',
         description: 'Uno de los edificios coloniales más antiguos de Granada, fundado en 1529. Hoy es un centro cultural con un museo que exhibe la historia de la ciudad y una importante colección de *estatuas precolombinas* de la Isla de Zapatera.',
-        image: "https://pub-dc88437b1eb54374b54af12eb03423a9.r2.dev/convento%20san%20francisco%20granada.webp",
+        image: "https://live.staticflickr.com/3234/2772165764_9bec1258ef_b.jpg",
         highlight: { primary: 'Granada, ', secondary: 'Convento San Francisco' },
         images: [
             "https://tse2.mm.bing.net/th/id/OIP.BQ006fxiH2ptCztl99ku2QHaFT?cb=12&rs=1&pid=ImgDetMain&o=7&rm=3",
@@ -132,7 +132,7 @@ export const places = [
         coordinate: { latitude: 11.930278, longitude: -85.957500 }, // 11°55′49″N 85°57′13″O - Coordenadas aproximadas de la Catedral
         title: 'Catedral de Granada',
         description: 'Elegante y colorida Catedral ubicada frente al Parque Central. Su estructura actual, reconstruida tras la quema de la ciudad por William Walker, es un *símbolo del resurgimiento* de Granada y un hito arquitectónico clave de la ciudad colonial.',
-        image: "https://pub-dc88437b1eb54374b54af12eb03423a9.r2.dev/catedral%20de%20granada.webp",
+        image: "https://tse1.mm.bing.net/th/id/OIP.Ev4767HHzkxn8AlSFT79cQHaFo?cb=12&rs=1&pid=ImgDetMain&o=7&rm=3",
         highlight: { primary: 'Granada, ', secondary: 'Catedral de Granada' },
         images: [
             "https://2.bp.blogspot.com/-ahwj5c5EIsI/U45bZIZvH7I/AAAAAAAAD-E/kfGa8uW1uKw/s1600/catedral-granada-nicaragua-1.jpg",
@@ -145,7 +145,7 @@ export const places = [
         coordinate: { latitude: 11.9328, longitude: -85.9472 }, // Coordenadas aproximadas de la Iglesia de Guadalupe
         title: 'Iglesia de Guadalupe',
         description: 'Ubicada al final de la Calle La Calzada, esta iglesia fue un punto de defensa clave durante la guerra contra los filibusteros en el siglo XIX. Su sencillez y ubicación a orillas del lago la hacen un *sitio de gran valor histórico* y religioso.',
-        image: "https://pub-dc88437b1eb54374b54af12eb03423a9.r2.dev/iglesia%20de%20guadalupe%20granada.webp",
+        image: "https://tse2.mm.bing.net/th/id/OIP.pu-oA-1L7BrEZ7logZ5kMgHaFc?cb=12&rs=1&pid=ImgDetMain&o=7&rm=3",
         highlight: { primary: 'Granada, ', secondary: 'Iglesia de Guadalupe' },
         images: [
             "https://c8.alamy.com/comp/GA615F/iglesia-de-guadalupe-granada-nicaragua-GA615F.jpg",
@@ -160,7 +160,7 @@ export const places = [
         coordinate: { latitude: 11.019167, longitude: -84.396667 }, // 11°01′09″N 84°23′48″O - Castillo de la Inmaculada Concepción
         title: 'Fortaleza de la Inmaculada Concepción',
         description: 'Histórica fortificación militar española, construida en 1673 para proteger la vía fluvial del río San Juan de los ataques piratas y expediciones extranjeras. Es un *símbolo de la soberanía nacional* y famosa por la gesta heroica de *Rafaela Herrera*.',
-        image: "https://pub-dc88437b1eb54374b54af12eb03423a9.r2.dev/fortaleza%20de%20la%20inmaculada%20concepcion.webp",
+        image: "https://c8.alamy.com/comp/ECJ7AB/nicaragua-rio-san-juan-department-the-little-village-of-el-castillo-ECJ7AB.jpg",
         highlight: { primary: 'El Castillo, ', secondary: 'Fortaleza de la Inmaculada Concepción' },
         images: [
             "https://c8.alamy.com/comp/ECJ7AB/nicaragua-rio-san-juan-department-the-little-village-of-el-castillo-ECJ7AB.jpg",
@@ -175,7 +175,7 @@ export const places = [
         coordinate: { latitude: 11.5303, longitude: -85.6450 }, // Coordenada aproximada del Museo El Ceibo
         title: 'Museo El Ceibo (Ometepe)',
         description: 'Ubicado en la Isla de Ometepe, alberga colecciones de *arqueología precolombina* con cerámicas y objetos líticos de la isla, y un *museo numismático* con billetes y monedas de la historia de Nicaragua. Un centro para entender la herencia indígena de Ometepe.',
-        image: "https://www.vivanicaragua.com.ni/contenido/archivos/2018/01/Museo-el-Ceibo-un-viaje-en-el-tiempo.jpg",
+        image: "https://www.mapanicaragua.com/wp-content/uploads/2020/09/Museo-de-Rivas_rivas_cultura_gal4.jpg",
         highlight: { primary: 'Ometepe, ', secondary: 'Museo El Ceibo' },
         images: [
             "https://th.bing.com/th/id/R.94cd3f4c34d2a62be2d95ee2abae23a0?rik=OWlp%2bpE7Ez9lgg&riu=http%3a%2f%2fmedia-cdn.tripadvisor.com%2fmedia%2fphoto-s%2f02%2f6d%2f13%2f23%2fmuseum-el-ceibo.jpg&ehk=gHeAYSFPW4U7hULwMHYspd17njdlDAkFMVshpqJL68Y%3d&risl=&pid=ImgRaw&r=0",
@@ -189,7 +189,7 @@ export const places = [
         coordinate: { latitude: 12.7237, longitude: -86.0886 }, // Coordenadas aproximadas de la Casa Natal
         title: 'Casa Natal Rubén Darío',
         description: 'Hogar donde nació el poeta universal *Félix Rubén García Sarmiento* (Rubén Darío). Declarada *Monumento Nacional* en 1940, es un sitio de peregrinación literaria y memoria cultural en Ciudad Darío, Matagalpa.',
-        image: "https://www.el19digital.com/files/articulos/323414.jpg",
+        image: "https://www.el19digital.com/files/articulos/402015.jpg",
         highlight: { primary: 'Ciudad Darío, ', secondary: 'Casa Natal Rubén Darío' },
         images: [
             "https://c8.alamy.com/comp/CEGMPH/casa-natal-ruben-dario-the-house-where-nicaraguan-poet-ruben-dario-CEGMPH.jpg",
@@ -202,7 +202,7 @@ export const places = [
         coordinate: { latitude: 11.979167, longitude: -86.096389 }, // Coordenadas aproximadas del Mercado de Artesanías de Masaya
         title: 'Antiguo Mercado de Masaya',
         description: 'Ubicado en un edificio histórico que fue un antiguo mercado y fuerte militar, hoy es el principal *centro de artesanías* de Nicaragua. Es un testimonio de la *cultura popular y la tradición artesanal* del país, especialmente de la cuna del folclore nicaragüense.',
-        image: "https://www.primerorden.com/wp-content/uploads/2024/03/Mercado-5-.jpeg",
+        image: "https://tse4.mm.bing.net/th/id/OIP.BaWhSEhtzBZFlgQjiM5zqwAAAA?cb=12&rs=1&pid=ImgDetMain&o=7&rm=3",
         highlight: { primary: 'Masaya, ', secondary: 'Mercado de Artesanías' },
         images: [
             "https://i.pinimg.com/originals/c9/35/4c/c9354ce2b4e32ebf7a8494537084aa0b.jpg",
@@ -218,7 +218,7 @@ export const places = [
         coordinate: { latitude: 11.2000, longitude: -85.2500 }, // Coordenada aproximada del Archipiélago
         title: 'Archipiélago de Solentiname',
         description: 'Grupo de islas en el Lago Cocibolca, famoso por ser el hogar de la *Comunidad de Artistas Primitivistas* fundada por el poeta Ernesto Cardenal. Su historia está ligada a la teología de la liberación y su *arte naif* refleja la belleza natural y la cultura local. El museo local resguarda parte de su historia.',
-        image: "https://www.visitanicaragua.com/wp-content/uploads/2023/01/Portada-Solentiname.jpg",
+        image: "https://tse4.mm.bing.net/th/id/OIP.Rmj82DEC-w7cA44Bwz4ZUAHaCx?cb=12&rs=1&pid=ImgDetMain&o=7&rm=3",
         highlight: { primary: 'Río San Juan, ', secondary: 'Solentiname' },
         images: [
             "https://www.mapanicaragua.com/wp-content/uploads/2024/04/Solentiname.jpg",
@@ -231,7 +231,7 @@ export const places = [
         coordinate: { latitude: 13.5936, longitude: -86.6111 }, // Coordenada aproximada del Cañón de Somoto
         title: 'Cañón de Somoto (Geoparque Río Coco)',
         description: 'Aunque es un accidente natural, el cañón ha sido fundamental en la vida y el desarrollo de la población de Somoto. La formación geológica es parte del *Geoparque Mundial de la UNESCO Río Coco* y atestigua la historia geológica de la Tierra en la región.',
-        image: "https://info-nicaragua.com/wp-content/uploads/2017/06/tama%C3%B1o-foto-principal-1024x492.jpg",
+        image: "https://images.ctfassets.net/wka5p55uxbqr/7q7zDs3SLB8g8h1QnxWqcc/216faff814bf1489cff70d698cbee85e/canion-somoto.jpg?w=1920&q=90",
         highlight: { primary: 'Somoto, ', secondary: 'Cañón de Somoto' },
         images: [
             "https://images.ctfassets.net/wka5p55uxbqr/7q7zDs3SLB8g8h1QnxWqcc/216faff814bf1489cff70d698cbee85e/canion-somoto.jpg?w=1920&q=90",
@@ -243,7 +243,7 @@ export const places = [
         coordinate: { latitude: 11.5306, longitude: -85.6669 }, // Coordenada aproximada de Punta Jesús María
         title: 'Punta Jesús María (Ometepe)',
         description: 'Famosa península en la Isla de Ometepe, conocida por su estrecha formación de arena que se adentra en el Lago Cocibolca, especialmente en verano. Aunque natural, es un *hito cultural* de la isla, un lugar de leyendas y una vista icónica de los *volcanes Concepción y Maderas*.',
-        image: "https://nextadventurenicaragua.com/wp-content/uploads/2019/02/punta-jesus-de-maria.jpg",
+        image: "https://bestregardsfromfar.com/wp-content/uploads/2016/03/ometepe-punta-jesus-maria.jpg",
         highlight: { primary: 'Ometepe, ', secondary: 'Punta Jesús María' },
         images: [
             "https://i.pinimg.com/originals/97/34/dd/9734dd63dacb3f9bfbbca10d21a15277.jpg",
